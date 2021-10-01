7bd59808a4724a34d1465575b8b84522d9f3c66d
erdodif <erdodif@gmail.com>
2021-10-01 14:12:12 +0200

    Szó elrejtése és megjelenítése

--- app/src/main/java/hu/petrik/akasztofa/MainActivity.kt
+++ app/src/main/java/hu/petrik/akasztofa/MainActivity.kt
@@ -1,39 +1,65 @@
 package hu.petrik.akasztofa
 
+import hu.petrik.akasztofa.databinding.ActivityMainBinding
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import kotlin.random.Random
+import android.widget.Toast
+import java.io.IOException
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
-import androidx.databinding.BaseObservable
-import androidx.databinding.Bindable
-import java.util.Observable
-import androidx.databinding.DataBindingUtil
-import hu.petrik.akasztofa.databinding.ActivityMainBinding
-import java.io.File
-import java.io.IOException
-import java.nio.charset.Charset
-import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     lateinit var bind: ActivityMainBinding
     lateinit var list: List<String>
-
     private var index = 0
     private val betuk: CharArray = "aábcdeéfghiíjklmnoóöőpqrstuúüűvwxyz".toCharArray()
+    private var allas = 0
+
+
+    private fun szoUres(szo: String): String {
+        val hatar = szo.length - 2
+        var kiad = ""
+        for (i in 0..hatar) {
+            kiad += "_" + " "
+        }
+        kiad += "_"
+        return kiad
+    }
 
     private fun init() {
-        try{
-            list = File("assets/szavak.txt").inputStream().readBytes()
-                .toString(Charsets.UTF_16).split("\n")
-            bind.textViewSzo.text = szoRandom()
-        }catch (e : IOException){
+        try {
+            list = assets.open("szavak.txt").bufferedReader(Charsets.UTF_8).readLines()
+            bind.textViewSzo.text = szoUres(szoRandom())
+        } catch (e: IOException) {
             Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
-            Log.d("Hiba",e.message.toString())
+            Log.d("Hiba", e.message.toString())
         }
     }
 
-    private fun szoRandom():String{
-        return list.get(Random.nextInt(list.size-1))
+    private fun allasKovetes() {
+        var melyik = R.drawable.akasztofa13
+        when (allas) {
+            0 -> melyik = R.drawable.akasztofa00
+            1 -> melyik = R.drawable.akasztofa01
+            2 -> melyik = R.drawable.akasztofa02
+            3 -> melyik = R.drawable.akasztofa03
+            4 -> melyik = R.drawable.akasztofa04
+            5 -> melyik = R.drawable.akasztofa05
+            6 -> melyik = R.drawable.akasztofa06
+            7 -> melyik = R.drawable.akasztofa07
+            8 -> melyik = R.drawable.akasztofa08
+            9 -> melyik = R.drawable.akasztofa09
+            10 -> melyik = R.drawable.akasztofa10
+            11 -> melyik = R.drawable.akasztofa11
+            12 -> melyik = R.drawable.akasztofa12
+        }
+        bind.imageViewKep.setImageResource(melyik)
+    }
+
+
+    private fun szoRandom(): String {
+        return list.get(Random.nextInt(list.size - 1))
     }
 
     private fun betuNovel() {
