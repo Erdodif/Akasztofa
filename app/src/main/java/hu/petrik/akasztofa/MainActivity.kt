f1579531f0bab809c5c4555cfcd2ee15eb1f32fb
erdodif <erdodif@gmail.com>
2021-10-01 14:52:29 +0200

    Tippelés elkezdése. Jelenleg sikertelen A betüváltoztatás jelenleg nem működik

--- app/src/main/java/hu/petrik/akasztofa/MainActivity.kt
+++ app/src/main/java/hu/petrik/akasztofa/MainActivity.kt
@@ -8,17 +8,21 @@ import android.widget.Toast
 import java.io.IOException
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AlertDialog
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
     lateinit var bind: ActivityMainBinding
     lateinit var list: List<String>
     private var index = 0
-    private val betuk: CharArray = "aábcdeéfghiíjklmnoóöőpqrstuúüűvwxyz".toCharArray()
+    private val betuk: String = "aábcdeéfghiíjklmnoóöőpqrstuúüűvwxyz".uppercase()
     private var allas = 0
+    private var aktualisSzo = ""
+    private var tippeltBetuk = ""
+    private var tippelhetoBetuk:CharArray = betuk.toCharArray()
 
-
-    private fun szoUres(szo: String): String {
-        val hatar = szo.length - 2
+    private fun getAktualisSzoUres(): String {
+        val hatar = aktualisSzo.length - 2
         var kiad = ""
         for (i in 0..hatar) {
             kiad += "_" + " "
@@ -28,9 +32,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun init() {
+        index = 0
+        allas = 0
+        aktualisSzo = ""
+        tippeltBetuk = ""
+        tippelhetoBetuk = betuk.toCharArray()
         try {
             list = assets.open("szavak.txt").bufferedReader(Charsets.UTF_8).readLines()
-            bind.textViewSzo.text = szoUres(szoRandom())
+            aktualisSzo = szoRandom().uppercase()
+            bind.textViewSzo.text = getAktualisSzoUres()
         } catch (e: IOException) {
             Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
             Log.d("Hiba", e.message.toString())
@@ -53,10 +63,20 @@ class MainActivity : AppCompatActivity() {
             10 -> melyik = R.drawable.akasztofa10
             11 -> melyik = R.drawable.akasztofa11
             12 -> melyik = R.drawable.akasztofa12
+            13 -> {
+                var alert = AlertDialog.Builder(this)
+                alert.setTitle("Vereség")
+                alert.setMessage("Veszített, szeretné újból megpróbálni?")
+                alert.setPositiveButton("igen"){_,_->
+                    init()
+                }
+                alert.setNegativeButton("nem"){_,_->
+                    exitProcess(0)
+                }
+            }
         }
         bind.imageViewKep.setImageResource(melyik)
     }
-
 
     private fun szoRandom(): String {
         return list.get(Random.nextInt(list.size - 1))
@@ -64,7 +84,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun betuNovel() {
         index++
-        if (index >= betuk.size) {
+        if (index == tippelhetoBetuk.size) {
             index = 0
         }
         betuallit()
@@ -73,13 +93,47 @@ class MainActivity : AppCompatActivity() {
     private fun betuCsokkent() {
         index--
         if (index < 0) {
-            index = betuk.size - 1
+            index = tippelhetoBetuk.size - 1
         }
         betuallit()
     }
 
     private fun betuallit() {
-        bind.textViewBetu.text = betuk[index].toString()
+        bind.textViewBetu.text = tippeltBetuk.get(index).toString()
+    }
+
+    fun getBetu(): Char {
+        return tippelhetoBetuk[index];
+    }
+
+    fun betuKeres(betu: Char): Boolean {
+        var talalat = false
+        var i = 0
+        while (i < aktualisSzo.length && aktualisSzo.get(i) == betu) {
+            i++
+        }
+        if (i < aktualisSzo.length) {
+            talalat = true
+        }
+        return talalat
+    }
+
+    fun betuElvesz(betu: Char) {
+        tippelhetoBetuk = tippelhetoBetuk.toString()
+            .replace(betu.toString(),"").toCharArray()
+    }
+
+    private fun tippel() {
+        if(betuKeres(getBetu())){
+            Toast.makeText(this, "'${getBetu()}' volt benne", Toast.LENGTH_SHORT).show()
+            betuElvesz(getBetu())
+        }
+        else{
+            allas--
+            allasKovetes()
+            Toast.makeText(this, "'${getBetu()}' nem volt benne", Toast.LENGTH_SHORT).show()
+            betuElvesz(getBetu())
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -96,6 +150,9 @@ class MainActivity : AppCompatActivity() {
             index = 0
             betuallit()
             true
+        }
+        bind.buttonTippel.setOnClickListener() {
+            tippel()
         }
     }
 }
